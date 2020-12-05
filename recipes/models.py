d6edaee5c8e145a0722f0d39b9cd9d325af825c9
Nicola Erario <nicola.erario@gmail.com>
2020-12-05 20:06:22 +0100

    recipes: adds recipe model (+)
    - parent_id is a recursive relationship: a recipe can be used
     as the basis for another recipe

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=400)
+    time = models.IntegerField()
+    description = models.TextField()
+    ingredients = models.CharField(max_length=1000)
+    parent_id = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
